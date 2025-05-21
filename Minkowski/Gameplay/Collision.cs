c8721157c288eeeb4cc8144ff69099a9a75af08e
anikyte <>
2025-05-20 22:43:07 -0400

    fixed radius detect logic

--- Minkowski/Gameplay/Collision.cs
+++ Minkowski/Gameplay/Collision.cs
@@ -23,7 +23,7 @@ public static class CollisionManager
 
     public static bool Collide(WorldlineEntity a, WorldlineEntity b)
     {
-        if (Vector2.DistanceSquared(a.Origin.ToVector2(), b.Origin.ToVector2()) < 4000)
+        if (Vector2.DistanceSquared(a.Origin.ToVector2(), b.Origin.ToVector2()) < Math.Pow(a.Radius + b.Radius, 2))
         {
             PathsD solution = Clipper.Intersect(new PathsD() {Transformations.Translate(a.Polygon, a.Origin.X, a.Origin.Y)}, new PathsD() {Transformations.Translate(b.Polygon, b.Origin.X, b.Origin.Y)}, FillRule.NonZero);
             return solution.Count > 0;
